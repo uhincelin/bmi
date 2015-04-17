8f31e134a377f53eb2125d36db12c1ddfff20fba
Ugo Hincelin <ugo.hincelin@gmail.com>
2015-04-17 15:31:12 -0400

    language

--- bmi.py
+++ bmi.py
@@ -1,4 +1,4 @@
 w = raw_input("enter weight in lbs: ") #pounds
 h = raw_input("enter height in inches: ") #inches
 bmi = (float(w)/float(h)**2)*703
-print round(bmi.1)
+print 'My bmi is %s' % (round(bmi,1))
